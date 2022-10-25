91165115f4c8933a47a9e4b673d96afb54cf64c4
Jordan González <jordanjg32@gmail.com>
2022-10-25 01:50:29 -0300

    Desafío Base de Datos

---
+++ Desafio_clase16 - Nuestra Primera Base de Datos/index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const path = require("path");
+const { formatMessage } = require("./utils/utils");
+const Products = require("./models/data");
+const SQLClient = require("./db/client/client");
+const dbConfig = require("./db/config");
+const SQLDBclient = require("./db/client/clientDB");
+const dbConfig2 = require("./db/configDB");
+
+const db = new SQLClient(dbConfig, "mensajes");
+const db2 = new SQLDBclient(dbConfig, "productos");
+// Servidor con Socket
+const { Server: HttpServer } = require("http");
+const { Server: SocketServer } = require("socket.io");
+
+const PORT = 8080 || process.env.PORT;
+const app = express();
+const httpServer = new HttpServer(app);
+const io = new SocketServer(httpServer);
+
+// Handlebars Config (Defino método Engine para HBS)
+const { engine } = require("express-handlebars");
+app.engine(
+	"hbs",
+	engine({
+		extname: "hbs",
+		defaultLayout: "main.hbs",
+		layoutsDir: path.resolve(__dirname, "./views/layouts"),
+		partialsDir: path.resolve(__dirname, "./views/partials"),
+	})
+);
+
+app.set("views", "./views"); // Referencia a la carpeta que contiene las vistas
+app.set("view engine", "hbs"); // Método - Motor de plantillas
+
+// Variables
+const messages = [];
+const users = [];
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./public"));
+
+const products = new Products();
+const { items } = products;
+
+// Routes
+app.get("/api/productos", (req, res) => {
+	res.json(items);
+});
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("*", (req, res) => {
+	res.status(404).send(`<h1>Path not found</h1>`);
+});
+
+app.post("/", async (req, res) => {
+    const data = req.body;
+	const { title, price, thumbnail } = req.body;
+	if (!title || !price || !thumbnail) {
+		return;
+	}
+	products.save(data);
+    await db2.saveProduct(data);
+    await db2.productAll();
+    res.redirect("/");
+});
+
+// Socket Events
+io.on("connection", (socket) => {
+	console.log("New client connection! 🟩");
+	console.log(socket.id);
+
+	// Getting all items
+	io.emit("items", [...items]);
+
+	// Getting all messages
+	io.emit('message', [...messages]);
+
+	// Welcome to chat
+	socket.on("new-user", (email) => {
+		const newUser = {
+			id: socket.id,
+			email: email
+		};
+		
+		users.push(newUser);
+	})
+
+	socket.on("new-message", async (msg) => {
+		const user = users.find((user) => user.id === socket.id);
+		const newMessage = formatMessage(socket.id, user.email, msg);
+		messages.push(newMessage);
+		products.saveMessage(user.email, msg, newMessage.time);
+
+        try {
+            await db.saveMessages({
+                email: user.email,
+                text: msg,
+                time: newMessage.time
+            });
+            const messagesLog = await db.selectAll();
+            console.log(messagesLog);
+        } catch (error) {
+            console.log(error.message);
+        }
+
+		io.emit("chat-message", newMessage);
+	});
+
+	socket.on("disconnect", () => {
+		io.emit("disc", socket.id);
+		console.log("Client disconnected 🟥");
+		console.log(socket.id);
+	});
+});
+
+// Listen
+const connectedServer = httpServer.listen(PORT, () => {
+	console.log(`Server is up and running on port ${PORT} 🚀`);
+});
+connectedServer.on("error", (error) => {
+	console.log(error);
+});
